362369d825351362169a0bf3b64de1a838a17cab
jhj6879 <jo39382355@gmail.com>
2024-08-28 11:21:49 +0900

    2024-08-28 [Account] {Controller} ::login ::joinAply
    {Dao}
    ::insertjoin
    
    {Dto}
    
    {join.html}
    {login.html}
    {application.yml}
    
    commited by "조현정"
    
    TODO
    -카카오 로그인 구현해야함......

--- src/main/java/com/eatmate/domain/dto/AccountDto.java
+++ src/main/java/com/eatmate/domain/dto/AccountDto.java
@@ -2,18 +2,22 @@ package com.eatmate.domain.dto;
 
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountDto {
 
     private Long account_id;
     private String email;    //로그인은 email로
-    private String nickName;
+    private String nick_name;
     private String password;
 
     //TODO : 추후 상세
