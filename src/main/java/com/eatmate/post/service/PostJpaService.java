873dd0a6d203499b67be28bdb77fd2edcae0bbfc
BonuKoo <evlogiaseio@gmail.com>
2024-09-02 09:57:24 +0900

    2024-09-02 09:57 AM
    [AccountTeam]
    {AccountTeam}
    -    //LEADER, PARTICIPANT
         @Column(name = "is_leader",nullable = false)
         private boolean isLeader;
    일반 회원인지, 방장인지 체크하는 컬럼
    String -> boolean으로 변경
    default : false
    true-> Leader (방장)
    false-> Participant (참가자)
    [Team]
    {Team}
    -    @JoinColumn(name = "account_id")
         private Account account_id;
         삭제
    
    [Post]
    {Post}
    {PostController}
    {PostJpaService}
    
    [HTML]
    
    Acked-by: 구본우

--- src/main/java/com/eatmate/post/service/PostJpaService.java
+++ src/main/java/com/eatmate/post/service/PostJpaService.java
@@ -1,7 +1,20 @@
 package com.eatmate.post.service;
 
+import com.eatmate.dao.repository.post.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class PostJpaService {
+
+    private final PostRepository postRepository;
+
+    /**
+     * 게시글 생성
+     */
+
+
 }
