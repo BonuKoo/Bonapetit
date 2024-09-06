4a6fb5b70d2b61a1faf9a5896820f665c942b927
Yooboong <yooboong22@gmail.com>
2024-09-06 14:31:17 +0900

    2024-09-06 14:31 [HTML, JS] {createPostForm.html} {createPostForm.js} {map.js} :: 팀 생성 시 맵 데이터 전송 구현
    commit by 장유석

--- src/main/resources/static/js/post/createPostForm.js
+++ src/main/resources/static/js/post/createPostForm.js
@@ -1,96 +1,121 @@
 //파일 폼 이미지 생성
-document.getElementById('imageFiles').addEventListener('change', function(event) {
-    const fileInput = event.target;
-    const files = fileInput.files;
-    const buttonsContainer = document.getElementById('buttonsContainer');
-    buttonsContainer.innerHTML = ''; // 기존 버튼 제거
+//document.getElementById('imageFiles').addEventListener('change', function(event) {
+//    const fileInput = event.target;
+//    const files = fileInput.files;
+//    const buttonsContainer = document.getElementById('buttonsContainer');
+//    buttonsContainer.innerHTML = ''; // 기존 버튼 제거
+//
+//    // 선택된 파일 수만큼 버튼 및 미리보기 이미지 추가
+//    for (let i = 0; i < files.length; i++) {
+//        const file = files[i];
+//
+//        // 파일 이름으로 버튼 생성
+//        const button = document.createElement('button');
+//        button.type = 'button';
+//        button.textContent = `파일 ${i + 1}: ${file.name}`;
+//        button.onclick = function() {
+//            alert(`파일 ${i + 1} 클릭됨: ${file.name}`);
+//        };
+//
+//        // 파일 미리보기 이미지 생성
+//        const img = document.createElement('img');
+//        img.style.maxWidth = '100px'; // 미리보기 이미지 크기 조정
+//        img.style.maxHeight = '100px';
+//        const reader = new FileReader();
+//        reader.onload = function(e) {
+//            img.src = e.target.result;
+//        };
+//        reader.readAsDataURL(file);
+//
+//        // 버튼과 미리보기 이미지를 컨테이너에 추가
+//        buttonsContainer.appendChild(button);
+//        buttonsContainer.appendChild(img);
+//    }
+//}
+//
+//function addFileInput() {
+//        // Check if the last file input is empty, don't add new input if it is
+//        const fileInputs = document.querySelectorAll('.file-input');
+//        if (fileInputs[fileInputs.length - 1].files.length === 0) {
+//            return;
+//        }
+//
+//        // Create a new file input element
+//        const newFileInput = document.createElement('input');
+//        const newId = `imageFiles${fileInputs.length}`;
+//        newFileInput.type = 'file';
+//        newFileInput.name = 'imageFiles';
+//        newFileInput.className = 'file-input';
+//        newFileInput.id = newId;
+//        newFileInput.multiple = true;
+//
+//        // Create a new label for the file input
+//        const newLabel = document.createElement('label');
+//        newLabel.htmlFor = newId;
+//        newLabel.textContent = '파일 업로드:';
+//
+//        // Append the new label and file input to the container
+//        fileInputsContainer.appendChild(newLabel);
+//        fileInputsContainer.appendChild(newFileInput);
+//    }
+//
+//    // Initially add one file input if there is only one present and empty
+//    if (document.querySelectorAll('.file-input').length === 1) {
+//        addFileInput();
+//    }
+//}
+//
+////해시태그 추가
+//function addHashtag() {
+//    var input = document.getElementById('hashtagInput');
+//    var hashtag = input.value.trim();
+//
+//    if (!hashtag.startsWith('#')) {
+//        alert('해시태그는 #으로 시작해야 합니다.');
+//        return;
+//    }
+//    if (hashtag.length < 2) {
+//        alert('해시태그는 최소 한 글자 이상이어야 합니다.');
+//        return;
+//    }
+//
+//    var li = document.createElement('li');
+//    li.textContent = hashtag;
+//    document.getElementById('hashtagList').appendChild(li);
+//    input.value = ''; // 입력 필드 초기화
+//    updateHiddenHashtagsField();
+//}
+//
+////추가할 때마다 리스트 하나씩 늘어나도록
+//function updateHiddenHashtagsField() {
+//    var hashtags = [];
+//    document.querySelectorAll('#hashtagList li').forEach(function(li) {
+//        hashtags.push(li.textContent);
+//    });
+//    document.getElementById('hashtagsField').value = hashtags.join(',');
+//}
 
-    // 선택된 파일 수만큼 버튼 및 미리보기 이미지 추가
-    for (let i = 0; i < files.length; i++) {
-        const file = files[i];
+// 생성 시 map, 방제, 설명 validation
+function createPostValidation(form) {
+    var x = form.querySelector('input[name="x"]').value.trim();
+    var y = form.querySelector('input[name="y"]').value.trim();
+    var teamName = form.querySelector('input[name="teamName"]').value.trim();
+    var description = form.querySelector('textarea[name="description"]').value.trim();
 
-        // 파일 이름으로 버튼 생성
-        const button = document.createElement('button');
-        button.type = 'button';
-        button.textContent = `파일 ${i + 1}: ${file.name}`;
-        button.onclick = function() {
-            alert(`파일 ${i + 1} 클릭됨: ${file.name}`);
-        };
-
-        // 파일 미리보기 이미지 생성
-        const img = document.createElement('img');
-        img.style.maxWidth = '100px'; // 미리보기 이미지 크기 조정
-        img.style.maxHeight = '100px';
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            img.src = e.target.result;
-        };
-        reader.readAsDataURL(file);
-
-        // 버튼과 미리보기 이미지를 컨테이너에 추가
-        buttonsContainer.appendChild(button);
-        buttonsContainer.appendChild(img);
+    if(x == '' || y == '') {
+        alert('위치를 설정하세요');
+        return false;
     }
-});
 
-function addFileInput() {
-        // Check if the last file input is empty, don't add new input if it is
-        const fileInputs = document.querySelectorAll('.file-input');
-        if (fileInputs[fileInputs.length - 1].files.length === 0) {
-            return;
-        }
-
-        // Create a new file input element
-        const newFileInput = document.createElement('input');
-        const newId = `imageFiles${fileInputs.length}`;
-        newFileInput.type = 'file';
-        newFileInput.name = 'imageFiles';
-        newFileInput.className = 'file-input';
-        newFileInput.id = newId;
-        newFileInput.multiple = true;
-
-        // Create a new label for the file input
-        const newLabel = document.createElement('label');
-        newLabel.htmlFor = newId;
-        newLabel.textContent = '파일 업로드:';
-
-        // Append the new label and file input to the container
-        fileInputsContainer.appendChild(newLabel);
-        fileInputsContainer.appendChild(newFileInput);
+    if(teamName == '') {
+        alert('방 제목을 입력하세요');
+        return false;
     }
 
-    // Initially add one file input if there is only one present and empty
-    if (document.querySelectorAll('.file-input').length === 1) {
-        addFileInput();
-    }
-});
-
-//해시태그 추가
-function addHashtag() {
-    var input = document.getElementById('hashtagInput');
-    var hashtag = input.value.trim();
-
-    if (!hashtag.startsWith('#')) {
-        alert('해시태그는 #으로 시작해야 합니다.');
-        return;
-    }
-    if (hashtag.length < 2) {
-        alert('해시태그는 최소 한 글자 이상이어야 합니다.');
-        return;
+    if(description == '') {
+        alert('설명을 입력하세요');
+        return false;
     }
 
-    var li = document.createElement('li');
-    li.textContent = hashtag;
-    document.getElementById('hashtagList').appendChild(li);
-    input.value = ''; // 입력 필드 초기화
-    updateHiddenHashtagsField();
-}
-
-//추가할 때마다 리스트 하나씩 늘어나도록
-function updateHiddenHashtagsField() {
-    var hashtags = [];
-    document.querySelectorAll('#hashtagList li').forEach(function(li) {
-        hashtags.push(li.textContent);
-    });
-    document.getElementById('hashtagsField').value = hashtags.join(',');
+    return true;
 }
