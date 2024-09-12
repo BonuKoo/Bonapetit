fe3b99fbd8052d6f474d8e1eff1823469a964df9
Yooboong <yooboong22@gmail.com>
2024-09-12 15:22:31 +0900

    2024-09-12 15:22 [HTML, JS] {createPostForm.html} {updatePostForm.html} {listPostForm.js} {updatePostForm.js} {map.js}
    [map]
    {MapVo}
    
    [post]
    {PostController}
    {PostJpaService}
    
    [domain]
    {Team}
    :: 채팅창 수정 기능 추가(지도 정보 변경)
    
    commit by 장유석

--- src/main/resources/static/js/post/listPostForm.js
+++ src/main/resources/static/js/post/listPostForm.js
@@ -1,38 +1,38 @@
-function updatePlaceholder() {
-    var searchType = document.getElementById("searchType").value;
-    var searchInput = document.getElementById("searchKeyword");
-
-    switch (searchType) {
-        case "location":
-            searchInput.placeholder = "Search by location...";
-            break;
-        case "title":
-            searchInput.placeholder = "Search by title...";
-            break;
-        case "date":
-            searchInput.type = "date";
-            break;
-        case "author":
-            searchInput.placeholder = "Search by author...";
-            searchInput.type = "text";
-            break;
-        case "teamName":
-            searchInput.placeholder = "Search by team name...";
-            searchInput.type = "text";
-            break;
-        case "foodKind":
-            searchInput.placeholder = "Search by food kind...";
-            searchInput.type = "text";
-            break;
-        default:
-            searchInput.placeholder = "Search...";
-    }
-}
-
-// 페이지 로드 시 초기화
-document.addEventListener("DOMContentLoaded", function() {
-    updatePlaceholder();
-});
+//function updatePlaceholder() {
+//    var searchType = document.getElementById("searchType").value;
+//    var searchInput = document.getElementById("searchKeyword");
+//
+//    switch (searchType) {
+//        case "location":
+//            searchInput.placeholder = "Search by location...";
+//            break;
+//        case "title":
+//            searchInput.placeholder = "Search by title...";
+//            break;
+//        case "date":
+//            searchInput.type = "date";
+//            break;
+//        case "author":
+//            searchInput.placeholder = "Search by author...";
+//            searchInput.type = "text";
+//            break;
+//        case "teamName":
+//            searchInput.placeholder = "Search by team name...";
+//            searchInput.type = "text";
+//            break;
+//        case "foodKind":
+//            searchInput.placeholder = "Search by food kind...";
+//            searchInput.type = "text";
+//            break;
+//        default:
+//            searchInput.placeholder = "Search...";
+//    }
+//}
+//
+//// 페이지 로드 시 초기화
+//document.addEventListener("DOMContentLoaded", function() {
+//    updatePlaceholder();
+//});
 
 // 리스트 검색
 function searchTeam() {
